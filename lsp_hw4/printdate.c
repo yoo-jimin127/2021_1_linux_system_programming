bcc030f5d81bc1806cb66ff8b59f62d110c82c49
yoo-jimin127 <dbwlals9936@naver.com>
2021-06-05 03:49:42 +0900

    3:49

--- lsp_hw4/printdate.c
+++ lsp_hw4/printdate.c
@@ -1,54 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
+
+void sigint_control(int signo);
 
 int main (int argc, char **argv) {
 	int timelimit = 0;
 	pid_t pid;
-	int exec_return = 0;
+	int status = 0;
+
+	if (argv[1] == NULL) {
+		timelimit = 0;
+	}
 
 	if (argv[1] != NULL) {
 		timelimit = atoi(argv[1]);
 	}
 
 	if (timelimit != 0) {
-		while(timelimit){
-			if ((pid = fork()) < 0) {
-				fprintf(stderr, "fork() error\n");
-				exit(1);
+		for (int i = 0; i < timelimit; i++) {
+			if ((pid = fork()) == 0) {
+				execl("/bin/date", "date", NULL, NULL);
 			}
 
-			else if ((pid = fork()) == 0) {
-				exec_return = execl("/bin/date", "/bin/date", NULL, (char *)0);
-				kill(pid, SIGINT);
-				if (exec_return == -1) {
-					fprintf(stderr, "failed the call of execl.\n");
-					exit(1);
-				}
-				sleep(1);
-				timelimit--;
-			}
-
-			if (timelimit <= 5) {
-				printf("printdate 종료 카운트다운 %d초 전\n", timelimit);
-			}
-		}
-	}
-
-	else if (timelimit == 0) {
-		while(1) {
-			if ((pid = fork()) < 0) {
-				fprintf(stderr, "fork() error at infinity roof\n");
-				exit(1);
-			}
-
-			else if ((pid = fork()) == 0) {
-				exec_return = execl("/bin/date", "/bin/date", NULL, (char *)0);
-				kill(pid, SIGINT);
-				if (exec_return == -1) {
-					fprintf(stderr, "failde the call of execl at infinity roof.\n");
+			else if (pid > 0) { 
+				if (wait(&status) != pid) {
+					fprintf(stderr, "wait() error\n");
 					exit(1);
 				}
 			}
@@ -57,6 +36,22 @@ int main (int argc, char **argv) {
 		}
 	}
 
+	else if (timelimit == 0) {
+		while(1) {
+			if ((pid = fork()) == 0) {
+				execl("/bin/date", "date", NULL, NULL);
+			}
+
+			else if (pid > 0) {
+				if (wait(&status) != pid) {
+					fprintf(stderr, "wait() error\n");
+					exit(1);
+				}
+			}
+
+			sleep(1);
+		}
+	}
 
 
 	return 0;
